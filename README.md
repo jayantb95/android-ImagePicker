12a2aca142125129d742c4452821e018c5c8b47c
Jayant <jayantb95@live.com>
2018-01-11 21:51:36 +0530

    initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+#Android Image Picker
+Sample Android application demonstrating the use of intents for picking an image and setting it in the ImageView.
---
+++ app/src/main/java/jayantb95/imagepicker/MainActivity.java
@@ -0,0 +1,63 @@
+package jayantb95.imagepicker;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int IMAGE_INTENT = 1;
+
+    private ImageView imageView;
+    private Button btnImagePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialize();
+        listener();
+    }
+
+    private void initialize() {
+        btnImagePicker = (Button) findViewById(R.id.btn_image_picker);
+        imageView = (ImageView) findViewById(R.id.imageview);
+    }
+
+    private void listener() {
+        btnImagePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImage();
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImage();
+            }
+        });
+    }
+
+    private void pickImage() {
+        Intent imageIntent = new Intent(Intent.ACTION_PICK);
+        imageIntent.setType("image/*");
+        startActivityForResult(imageIntent, IMAGE_INTENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_INTENT && resultCode == RESULT_OK) {
+            Bitmap bmp = ImagePicker.getImageFromResult(MainActivity.this, resultCode, data);
+            imageView.setImageBitmap(bmp);
+
+        }
+    }
+}
